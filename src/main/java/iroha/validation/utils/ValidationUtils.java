57eb57e6f4eb2c67a508b3a995a9ac4459b47272
Bogdan Mingela <freekilluau@gmail.com>
2019-02-12 09:28:50 +0300

    Changed Utils to interface

--- src/main/java/iroha/validation/utils/ValidationUtils.java
+++ src/main/java/iroha/validation/utils/ValidationUtils.java
@@ -3,17 +3,13 @@ package iroha.validation.utils;
 import iroha.protocol.TransactionOuterClass.Transaction;
 import jp.co.soramitsu.iroha.java.Utils;
 
-public class ValidationUtils {
+public interface ValidationUtils {
 
-  private ValidationUtils() {
-    throw new IllegalStateException("Util class cannot be instantiated");
-  }
-
-  public static String getTxAccountId(final Transaction transaction) {
+  static String getTxAccountId(final Transaction transaction) {
     return transaction.getPayload().getReducedPayload().getCreatorAccountId();
   }
 
-  public static String hexHash(Transaction transaction) {
+  static String hexHash(Transaction transaction) {
     return Utils.toHex(Utils.hash(transaction));
   }
 }
