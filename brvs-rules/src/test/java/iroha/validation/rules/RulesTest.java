66b4494d5f3fca199cfc69c00ddb36690801e834
Bogdan Mingela <freekilluau@gmail.com>
2019-02-15 10:33:19 +0300

    Multiproject build

--- brvs-rules/src/test/java/iroha/validation/rules/RulesTest.java
+++ brvs-rules/src/test/java/iroha/validation/rules/RulesTest.java
@@ -1,4 +1,4 @@
-package iroha.validation.rules;
+package test.java.iroha.validation.rules;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
