4efff4f3b68a82447847504065c122316b8abf25
Bogdan Mingela <freekilluau@gmail.com>
2019-03-29 11:46:16 +0300

    Added files keys import

--- brvs-core/src/main/java/iroha/validation/utils/ValidationUtils.java
+++ brvs-core/src/main/java/iroha/validation/utils/ValidationUtils.java
@@ -4,10 +4,12 @@ import com.google.common.collect.ImmutableList;
 import iroha.protocol.TransactionOuterClass.Transaction;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.KeyPair;
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.bind.DatatypeConverter;
 import jp.co.soramitsu.crypto.ed25519.Ed25519Sha3;
 import jp.co.soramitsu.iroha.java.Utils;
 
@@ -37,13 +39,28 @@ public interface ValidationUtils {
     return new String(Files.readAllBytes(Paths.get(keyPath)));
   }
 
-  static List<KeyPair> generateKeypairs(int amount) {
+  static List<KeyPair> generateOrImportKeypairs(int amount, String path) throws IOException {
     if (amount < 1) {
       throw new IllegalArgumentException("Amount must be more than zero");
     }
+    final Path keysPath = Paths.get(path);
     List<KeyPair> keyPairs = new ArrayList<>(amount);
+    Files.createDirectories(keysPath);
     for (int i = 0; i < amount; i++) {
-      keyPairs.add(generateKeypair());
+      final Path pubPath = keysPath.resolve("key" + i + ".pub");
+      final Path privPath = keysPath.resolve("key" + i + ".priv");
+      if (pubPath.toFile().exists() && privPath.toFile().exists()) {
+        final String pubKey = readKey(pubPath.toString());
+        final String privKey = readKey(privPath.toString());
+        keyPairs.add(Utils.parseHexKeypair(pubKey, privKey));
+      } else {
+        final KeyPair keypair = generateKeypair();
+        keyPairs.add(keypair);
+        Files.write(pubPath,
+            DatatypeConverter.printHexBinary(keypair.getPublic().getEncoded()).getBytes());
+        Files.write(privPath,
+            DatatypeConverter.printHexBinary(keypair.getPrivate().getEncoded()).getBytes());
+      }
     }
     return ImmutableList.copyOf(keyPairs);
   }
