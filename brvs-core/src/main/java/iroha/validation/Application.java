283b4eb97de71d272ba7d98f5dc2ee8a29717dd1
Bogdan Mingela <freekilluau@gmail.com>
2019-04-18 15:12:32 +0300

    Added customizable endpoint port (#55)

--- brvs-core/src/main/java/iroha/validation/Application.java
+++ brvs-core/src/main/java/iroha/validation/Application.java
@@ -10,12 +10,17 @@ import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Application {
 
-  private static final String BASE_URI = "http://0.0.0.0:8081/brvs/rest";
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+  private static final String BRVS_PORT_BEAN_NAME = "brvsPort";
+  private static final int DEFAULT_PORT = 8080;
+  private static final String BASE_URI_FORMAT = "http://0.0.0.0:%d/brvs/rest";
 
   public static void main(String[] args) {
     if (args.length == 0) {
@@ -38,6 +43,23 @@ public class Application {
       }
     });
     resourceConfig.property(ServerProperties.OUTBOUND_CONTENT_LENGTH_BUFFER, 0);
-    GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
+
+    int port = getPort(context);
+    logger.info("Going to establish HTTP server on port " + port);
+    GrizzlyHttpServerFactory
+        .createHttpServer(URI.create(String.format(BASE_URI_FORMAT, port)), resourceConfig);
+  }
+
+  private static int getPort(AbstractApplicationContext context) {
+    int portBean = DEFAULT_PORT;
+    try {
+      portBean = Integer.parseInt(context.getBean(BRVS_PORT_BEAN_NAME, String.class));
+      if (portBean < 0 || portBean > 65535) {
+        throw new Exception("Got port out of range: " + portBean);
+      }
+    } catch (Exception e) {
+      logger.warn("Couldn't read the port. Reason: " + e.getMessage());
+    }
+    return portBean;
   }
 }
