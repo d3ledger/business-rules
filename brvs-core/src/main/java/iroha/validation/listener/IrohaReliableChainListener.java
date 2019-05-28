a721b709343239819dba1b77e5ff04b76d49d4bb
Mingela <freekilluau@gmail.com>
2019-05-28 15:30:29 +0300

    Added copyright

--- brvs-core/src/main/java/iroha/validation/listener/IrohaReliableChainListener.java
+++ brvs-core/src/main/java/iroha/validation/listener/IrohaReliableChainListener.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 package iroha.validation.listener;
 
 import com.google.common.base.Strings;
--- brvs-rules/src/main/java/iroha/validation/rules/impl/NewBrvsRule.java
+++ brvs-rules/src/main/java/iroha/validation/rules/impl/NewBrvsRule.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 package iroha.validation.rules.impl;
 
 import com.google.gson.JsonElement;
