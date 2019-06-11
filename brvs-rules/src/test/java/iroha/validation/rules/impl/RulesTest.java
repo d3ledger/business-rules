c6f491a4712c139ea69a47adf83dde7a020c9174
Mingela <freekilluau@gmail.com>
2019-06-11 17:14:15 +0300

    Fixed test

--- brvs-rules/src/test/java/iroha/validation/rules/impl/RulesTest.java
+++ brvs-rules/src/test/java/iroha/validation/rules/impl/RulesTest.java
@@ -89,7 +89,7 @@ class RulesTest {
   private void initRestrictedKeysRuleTest(boolean bad) {
     init();
     keyPair = new Ed25519Sha3().generateKeypair();
-    rule = new RestrictedKeysRule(Collections.singletonList(keyPair));
+    rule = new RestrictedKeysRule("", Collections.singletonList(keyPair));
     final RemoveSignatory removeSignatory = mock(RemoveSignatory.class);
     final String value = bad ? Utils.toHex(keyPair.getPublic().getEncoded()) : "";
     when(removeSignatory.getPublicKey()).thenReturn(value);
