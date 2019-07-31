6abbeb4f625189f5777df6bd3dab754289429092
Alexey <Alexey.N.Chernyshov@gmail.com>
2019-07-31 14:44:36 +0300

    Feature/bump iroha 1.1.0 (#115)
    * bump Iroha-java to 6.0.0

--- brvs-core/src/main/java/iroha/validation/transactions/provider/impl/AccountManager.java
+++ brvs-core/src/main/java/iroha/validation/transactions/provider/impl/AccountManager.java
@@ -121,7 +121,8 @@ public class AccountManager implements UserQuorumProvider, RegistrationProvider 
           .getAsJsonObject()
           .getAsJsonObject(brvsAccountId);
 
-      if (keyNode.isJsonNull() || keyNode.get(userSignatoriesAttribute).isJsonNull()) {
+      if (keyNode == null || keyNode.isJsonNull()
+          || keyNode.size() == 0 || keyNode.get(userSignatoriesAttribute).isJsonNull()) {
         logger.warn("Account detail is not set for account: " + targetAccount);
         return Collections.emptySet();
       }
