88264dcb1d20e551f4238e2797510bb40ceb97f3
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2019-09-03 10:53:21 +0300

    add null check to DTO
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- brvs-core/src/main/java/iroha/validation/rest/dto/BinaryTransaction.java
+++ brvs-core/src/main/java/iroha/validation/rest/dto/BinaryTransaction.java
@@ -6,19 +6,23 @@
 package iroha.validation.rest.dto;
 
 import java.util.Base64;
+import java.util.Objects;
 
 /**
  * Binary representation of protobuf transaction in hex.
  */
 public final class BinaryTransaction {
 
+  public final String hexString;
+
   public BinaryTransaction(String hexString) {
+    Objects.requireNonNull(hexString);
     this.hexString = hexString;
   }
 
   public BinaryTransaction(byte[] bytes) {
+    Objects.requireNonNull(bytes);
     hexString = Base64.getEncoder().encodeToString(bytes);
   }
 
-  public final String hexString;
 }
