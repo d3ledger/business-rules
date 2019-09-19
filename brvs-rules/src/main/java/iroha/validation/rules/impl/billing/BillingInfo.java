f80638fe4b639167027c7384f03ee8b122a8a806
Mingela <freekilluau@gmail.com>
2019-09-19 12:44:09 +0300

    PR issues

--- brvs-rules/src/main/java/iroha/validation/rules/impl/billing/BillingInfo.java
+++ brvs-rules/src/main/java/iroha/validation/rules/impl/billing/BillingInfo.java
@@ -130,11 +130,11 @@ public class BillingInfo {
 
   @Override
   public boolean equals(Object other) {
-    if (other == this) {
-      return true;
-    }
     if (other == null || !this.getClass().isAssignableFrom(other.getClass())) {
       return false;
+    }
+    if (other == this) {
+      return true;
     }
     BillingInfo otherObj = (BillingInfo) other;
     return otherObj.asset.equals(this.asset)
