e484535b6783cb45ea780318cd427011142891ff
Mingela <freekilluau@gmail.com>
2019-10-10 17:48:36 +0300

    Removed bad ack

--- brvs-core/src/main/java/iroha/validation/rules/RuleMonitor.java
+++ brvs-core/src/main/java/iroha/validation/rules/RuleMonitor.java
@@ -78,27 +78,22 @@ public class RuleMonitor {
       return;
     }
     logger.info("Starting rules updates monitoring");
-    irohaChainListener.getBlockStreaming().observeOn(scheduler).subscribe(blockSubscription -> {
-          try {
-            blockSubscription.getBlock().getBlockV1().getPayload().getTransactionsList().stream()
-                .map(transaction -> transaction.getPayload().getReducedPayload())
+    irohaChainListener.getBlockStreaming().observeOn(scheduler).subscribe(blockSubscription ->
+        blockSubscription.getBlock().getBlockV1().getPayload().getTransactionsList().stream()
+            .map(transaction -> transaction.getPayload().getReducedPayload())
+            .filter(
+                reducedPayload -> reducedPayload.getCreatorAccountId().equals(setterAccountId)
+            )
+            .map(ReducedPayload::getCommandsList)
+            .forEach(commands -> commands.stream()
+                .filter(Command::hasSetAccountDetail)
+                .map(Command::getSetAccountDetail)
                 .filter(
-                    reducedPayload -> reducedPayload.getCreatorAccountId().equals(setterAccountId)
+                    setAccountDetail -> setAccountDetail.getAccountId()
+                        .equals(settingsAccountId)
                 )
-                .map(ReducedPayload::getCommandsList)
-                .forEach(commands -> commands.stream()
-                    .filter(Command::hasSetAccountDetail)
-                    .map(Command::getSetAccountDetail)
-                    .filter(
-                        setAccountDetail -> setAccountDetail.getAccountId()
-                            .equals(settingsAccountId)
-                    )
-                    .forEach(this::processUpdate)
-                );
-          } finally {
-            blockSubscription.getAcknowledgment().ack();
-          }
-        }
+                .forEach(this::processUpdate)
+            )
     );
     isStarted = true;
   }
